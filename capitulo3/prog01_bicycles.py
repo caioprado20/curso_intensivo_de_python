e08668b311068869d05aaed47212e518dbd065db
caio-prado <caioprado20@gmail.com>
2026-09-23 22:38:19 -0300

    ..

---
+++ capitulo3/prog01_bicycles.py
@@ -0,0 +1,4 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialzed']
+print(bicycles); print(bicycles[0]); print(bicycles[0].upper()) 
+message = f"My first bycycle was a {bicycles[0].title()},"
+print(message)
---
+++ capitulo3/prog02_listas_amigos.py
@@ -0,0 +1,14 @@
+amigos = ["naiana", "mory", "abilio"]
+print(amigos[0]); 
+print(amigos[1]); 
+print(amigos[2].title());
+
+print (f"oi {amigos[0]}, gosto muito de vc")
+
+transportes_meios = ["carro", 'moto', 'meupé']
+print(transportes_meios)
+print(f"gostaria de nunca andar de {transportes_meios[1]}")
+
+
+
+
